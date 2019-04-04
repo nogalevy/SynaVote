ddc95b421393a42f7b793ea2c02f35ca7198bdab
shirarab <broomonster@gmail.com>
2019-04-04 13:33:48 +0300

    like or not page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LikeOrNotPage from './LikeOrNotPage.js'
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <LikeOrNotPage />
-        </header>
+        <LikeOrNotPage />
       </div>
     );
   }
--- src/LikeOrNotPage.js
+++ src/LikeOrNotPage.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import kobimarimi from './images/kobimarimi.jpg';
+import './LikeOrNotPage.css';
 
 class LikeOrNotPage extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        SynaVote
-        </header>
+      <div>
+
+        <div>
+          <img className="song-image" src={kobimarimi} alt="kobimarimi" />
+        </div>
+        <div className="icons-container">
+          <div className="heart icon">
+            <i className="fas fa-heart fa-5x" />
+          </div>
+          <div className="x icon">
+            <i className="fas fa-times fa-5x" />
+          </div>
+        </div>
+
       </div>
     );
   }
